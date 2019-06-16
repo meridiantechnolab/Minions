3ce9b7b24c0fe77728311b322fa632e0bf346875
MeridianTechnoLab <info@meridiantechnolab.com>
2019-06-16 11:01:22 +1000

    Updated Minion2

--- Minion2/Minion2.ino
+++ Minion2/Minion2.ino
@@ -1,136 +1,136 @@
 #include <Minion.h>
 
 //             LHD, RHD, LHU, RHU, LHF, RHF, G_R ,  G_L , K_F     , K_B
-Minion minion1(460, 105, 120, 470, 270, 275, 1.70, -1.70, 1770/100, 1785/100);
+Minion minion2(450, 105, 150, 490, 300, 275, 1.70, -1.70, 1770/100, 1785/100);
 
 void Hands() {
-   minion1.HandsUp();
-   minion1.pause(400);
-   minion1.RightHandForward();
-   minion1.pause(400);
+   minion2.HandsUp();
+   minion2.pause(400);
+   minion2.RightHandForward();
+   minion2.pause(400);
 
    for (int i=1; i <= 4; i++){
-      minion1.RightHandUp();
-      minion1.LeftHandForward();
-      minion1.pause(400);
-      minion1.RightHandForward();
-      minion1.LeftHandUp();
-      minion1.pause(400);
+      minion2.RightHandUp();
+      minion2.LeftHandForward();
+      minion2.pause(400);
+      minion2.RightHandForward();
+      minion2.LeftHandUp();
+      minion2.pause(400);
    }
 
-   minion1.HandsDown();
-   minion1.pause(800);
+   minion2.HandsDown();
+   minion2.pause(800);
    //RadioActivation();
-   minion1.RightHandUp();
-   minion1.pause(800);
+   minion2.RightHandUp();
+   minion2.pause(800);
 
    for (int i=1; i <= 4; i++){
-      minion1.RightHandDown();
-      minion1.LeftHandUp();
-      minion1.pause(800);
-      minion1.RightHandUp();
-      minion1.LeftHandDown();
-      minion1.pause(800);
+      minion2.RightHandDown();
+      minion2.LeftHandUp();
+      minion2.pause(800);
+      minion2.RightHandUp();
+      minion2.LeftHandDown();
+      minion2.pause(800);
    }
 
-   minion1.HandsDown();
-   minion1.pause(800);
+   minion2.HandsDown();
+   minion2.pause(800);
 
    for (int j = 1; j <= 3; j++) {
-      minion1.GyroTurnRightDegrees(100);
-      minion1.RobotStop(500);
-      minion1.RightHandUp();
-      minion1.pause(400);
+      minion2.GyroTurnRightDegrees(100);
+      minion2.RobotStop(500);
+      minion2.RightHandUp();
+      minion2.pause(400);
       for (int i=1; i <= 2; i++){
-         minion1.RightHandDown();
-         minion1.LeftHandUp();
-         minion1.pause(400);
-         minion1.RightHandUp();
-         minion1.LeftHandDown();
-         minion1.pause(400);
+         minion2.RightHandDown();
+         minion2.LeftHandUp();
+         minion2.pause(400);
+         minion2.RightHandUp();
+         minion2.LeftHandDown();
+         minion2.pause(400);
       }
-      minion1.HandsDown();
-      minion1.pause(800);
+      minion2.HandsDown();
+      minion2.pause(800);
    }
-   minion1.GyroTurnRightDegrees(100);
-   minion1.RobotStop(500);
+   minion2.GyroTurnRightDegrees(100);
+   minion2.RobotStop(500);
 }
 
 void Happy() {
-   minion1.start();
+   minion2.start();
    delay(600);
 
    // Begining
-   minion1.RobotMoveForwardCM(50);
-   minion1.RobotMoveForwardCM(50);
-   minion1.RobotStop(500);
-   minion1.HandsUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(500);
-   minion1.GyroTurnLeftDegrees(90);
-   minion1.RobotStop(500);
-   minion1.HandsUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(500);
-   minion1.RobotMoveForwardCM(30);
-   minion1.RobotStop(500);
-   minion1.RightHandUp();
-   minion1.pause(500);
-   minion1.LeftHandUp();
-   minion1.RightHandDown();
-   minion1.pause(500);
-   minion1.LeftHandDown();
-   minion1.RightHandUp();
-   minion1.pause(500);
-   minion1.LeftHandUp();
-   minion1.RightHandDown();
-   minion1.pause(500);
-   minion1.LeftHandDown();
-   minion1.pause(500);
-   minion1.GyroTurnRightDegrees(90);
-   minion1.RobotStop(500);
-   minion1.RobotMoveBackCM(50);
-   minion1.RobotStop(500);
-   minion1.GyroTurnRightDegrees(90);
-   minion1.RobotStop(100);
+   minion2.RobotMoveForwardCM(50);
+   minion2.RobotMoveForwardCM(50);
+   minion2.RobotStop(500);
+   minion2.HandsUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(500);
+   minion2.GyroTurnLeftDegrees(90);
+   minion2.RobotStop(500);
+   minion2.HandsUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(500);
+   minion2.RobotMoveForwardCM(30);
+   minion2.RobotStop(500);
+   minion2.RightHandUp();
+   minion2.pause(500);
+   minion2.LeftHandUp();
+   minion2.RightHandDown();
+   minion2.pause(500);
+   minion2.LeftHandDown();
+   minion2.RightHandUp();
+   minion2.pause(500);
+   minion2.LeftHandUp();
+   minion2.RightHandDown();
+   minion2.pause(500);
+   minion2.LeftHandDown();
+   minion2.pause(500);
+   minion2.GyroTurnRightDegrees(90);
+   minion2.RobotStop(500);
+   minion2.RobotMoveBackCM(50);
+   minion2.RobotStop(500);
+   minion2.GyroTurnRightDegrees(90);
+   minion2.RobotStop(100);
    //BluetoothActivation();
    delay(1200);
-   minion1.HandsUp();
-   minion1.pause(250);
-   minion1.GyroTurnRightDegrees(90);
-   minion1.RobotStop(500);
-   minion1.HandsDown();
+   minion2.HandsUp();
+   minion2.pause(250);
+   minion2.GyroTurnRightDegrees(90);
+   minion2.RobotStop(500);
+   minion2.HandsDown();
    //BluetoothActivation();
    delay(1000);
-   minion1.RobotMoveForwardCM(50);
+   minion2.RobotMoveForwardCM(50);
    delay(300);
-   minion1.RobotStop(100);
+   minion2.RobotStop(100);
    delay(500);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(2000);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(500);
-   minion1.RobotMoveBackCM(50);
+   minion2.RobotMoveBackCM(50);
    delay(300);
-   minion1.RobotStop(100);
+   minion2.RobotStop(100);
    //BluetoothActivation();
    delay(1000);
 
    // Turns 7s
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(500);
-   minion1.HandsDown();
-   minion1.pause(500);
-   minion1.GyroTurnRightDegrees(90);
-   minion1.RobotStop(1000);
-   minion1.HandsUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(500);
-   minion1.GyroTurnRightDegrees(90);
-   minion1.RobotStop(500);
+   minion2.HandsDown();
+   minion2.pause(500);
+   minion2.GyroTurnRightDegrees(90);
+   minion2.RobotStop(1000);
+   minion2.HandsUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(500);
+   minion2.GyroTurnRightDegrees(90);
+   minion2.RobotStop(500);
 
    // Hands 23s
    Hands();
@@ -138,76 +138,76 @@ void Happy() {
    delay(1000);
 
    // Final moves
-   minion1.RobotMoveForwardCM(35);
-   minion1.RobotStop(500);
-   minion1.HandsUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(1000);
-   minion1.RobotMoveBackCM(85);
-   minion1.RobotStop(500);
+   minion2.RobotMoveForwardCM(35);
+   minion2.RobotStop(500);
+   minion2.HandsUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(1000);
+   minion2.RobotMoveBackCM(85);
+   minion2.RobotStop(500);
    delay(1000);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(700);
-   minion1.HandsDown();
-   minion1.pause(1000);
-   minion1.RobotMoveForwardCM(60);
-   minion1.RobotStop(500);
-   minion1.HandsUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(1000);
+   minion2.HandsDown();
+   minion2.pause(1000);
+   minion2.RobotMoveForwardCM(60);
+   minion2.RobotStop(500);
+   minion2.HandsUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(1000);
    delay(000);
    
    // 8 sec
-   minion1.GyroTurnLeftDegrees(100);
-   minion1.RobotStop(500);
-   minion1.RightHandForward();
-   minion1.pause(500);
-   minion1.LeftHandForward();
-   minion1.RightHandDown();
-   minion1.pause(500);
-   minion1.LeftHandDown();
-   minion1.RightHandForward();
-   minion1.pause(500);
-   minion1.LeftHandUp();
-   minion1.pause(500);
-   minion1.RightHandUp();
-   minion1.LeftHandForward();
-   minion1.pause(500);
-   minion1.RightHandForward();
-   minion1.LeftHandUp();
-   minion1.pause(500);
-   minion1.HandsDown();
-   minion1.pause(500);
-   minion1.GyroTurnRightDegrees(100);
-   minion1.RobotStop(500);
-   minion1.HandsForward();
-   minion1.pause(200);
+   minion2.GyroTurnLeftDegrees(100);
+   minion2.RobotStop(500);
+   minion2.RightHandForward();
+   minion2.pause(500);
+   minion2.LeftHandForward();
+   minion2.RightHandDown();
+   minion2.pause(500);
+   minion2.LeftHandDown();
+   minion2.RightHandForward();
+   minion2.pause(500);
+   minion2.LeftHandUp();
+   minion2.pause(500);
+   minion2.RightHandUp();
+   minion2.LeftHandForward();
+   minion2.pause(500);
+   minion2.RightHandForward();
+   minion2.LeftHandUp();
+   minion2.pause(500);
+   minion2.HandsDown();
+   minion2.pause(500);
+   minion2.GyroTurnRightDegrees(100);
+   minion2.RobotStop(500);
+   minion2.HandsForward();
+   minion2.pause(200);
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(150);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(150);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(100);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(100);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(50);
-   minion1.HandsDown();
+   minion2.HandsDown();
    delay(50);
-   minion1.HandsUp();
+   minion2.HandsUp();
    delay(50);
-   minion1.HandsDown();
+   minion2.HandsDown();
    
 
 }
