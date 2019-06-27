76f54b04ede4fd756c681285b8bdfd4128906855
Sonk17 <sonksgame17@gmail.com>
2019-06-27 17:08:34 +1000

    Updated script and Labelled prpgram

--- Minion_demo/Minion_demo.ino
+++ Minion_demo/Minion_demo.ino
@@ -18,28 +18,39 @@
 Minion minion1(460, 105, 120, 470, 270, 275, 1.70, -1.70, 1770/100, 1785/100, 990);
   
 void Demo (){
+  //Init
   minion1.start();
   delay(500);
+
+  //Ultrasonic Activation
   minion1.RobotMoveForwardCM(50);
   delay(500);
   minion1.RobotStop(500);
   minion1.pause(500);
+
+   //Gyro turning
   minion1.UltraSonicActivation();
   delay(500);
   minion1.GyroTurnRightDegrees(90);
   minion1.RobotStop(500);
   minion1.GyroTurnLeftDegrees(90);
   minion1.RobotStop(500);
+  
+  //Colour Sensor
   minion1.UltraSonicActivation();
   delay(500);
   minion1.RobotMoveForwardCM(50);
   minion1.pause(3000);
   delay(1500);
   minion1.RobotStop(500);
+
+  //Show LED
   minion1.UltraSonicActivation();
   delay(500);
   minion1.start();
   minion1.pause(500);
+
+  //Obstacle avoidance
   minion1.UltraSonicActivation();
   delay(500);
   minion1.RobotMoveForwardCM(100);
